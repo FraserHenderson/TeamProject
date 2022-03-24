53c13904ada40946dcc016f65142dd7b2455a77a
“Fraser <2557617h@student.gla.ac.uk>
2022-03-24 12:07:54 +0000

    commit so nothing gets lost 1 Fraser and Matas

--- rango/forms.py
+++ rango/forms.py
@@ -3,37 +3,6 @@ from rango.models import Page, Category
 from django.contrib.auth.models import User
 from rango.models import UserProfile, Medium
 
-class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
-                           help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-
-    class Meta:
-        model = Category
-        fields = ('name',)
-        
-
-class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
-    class Meta:
-        model = Page
-        exclude = ('category',)
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
-            
-        return cleaned_data
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -58,7 +27,7 @@ class MediumForm(forms.ModelForm):
     #medium_category = forms.ForeignKey(MediaCategory, on_delete=models.CASCADE, blank=True, null=True)
     class Meta:
         model = Medium
-        fields = ('name', 'description', 'thumbnail', )
+        fields = ('name', 'description', 'thumbnail',)
     
                              
         
