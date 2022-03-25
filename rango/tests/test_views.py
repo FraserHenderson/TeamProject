c2d76c76f7620ec2ad45297082ebfd7e52b08146
James <2509941b@student.gla.ac.uk>
2022-03-25 14:44:22 +0000

    added ProfileView test - James

--- rango/tests/test_views.py
+++ rango/tests/test_views.py
@@ -36,7 +36,7 @@ class IndexViewTest(TestCase):
         expected_medium_order = list(Medium.objects.order_by('-publish_date')[:5])
         self.assertEqual(expected_medium_order, list(contextDict['posts']))
 
-        expected_media_category_order = list(MediaCategory.objects.order_by('name'))
+        expected_media_category_order = list(MediaCategory.objects.filter(approved=True).order_by('name'))
         self.assertEqual(expected_media_category_order, list(contextDict['media_categories']))
 
         expected_user_entity_order = list(UserEntity.objects.order_by('name'))
@@ -88,6 +88,47 @@ class RegisterProfileViewTest(TestCase):
         self.client.login(username="testuser", password='123')
         response = self.client.get(reverse('rango:register_profile'))
         self.assertTemplateUsed(response, 'rango/profile_registration.html')
+
+class ProfileViewTest(TestCase):
+
+    def test_profile_view_logged_out(self):
+        user = create_user_object()
+        name = user.username
+        response = self.client.get(reverse('rango:profile', kwargs={'username': name}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_profile_view_logged_in(self):
+        user = create_user_object()
+        name = user.username
+        self.client.login(username="testuser", password='123')
+        response = self.client.get(reverse('rango:profile', kwargs={'username':name}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        user = create_user_object()
+        name = user.username
+        self.client.login(username="testuser", password='123')
+        response = self.client.get(reverse('rango:profile', kwargs={'username':name}))
+        self.assertTemplateUsed(response, 'rango/profile.html')
+
+    def test_profile_view_context_dictionary(self):
+
+        user = create_user_object()
+        name = user.username
+        self.client.login(username="testuser", password='123')
+        self.response = self.client.get(reverse('rango:profile', kwargs={'username': name}))
+
+        self.assertTrue('userprofile' in self.response.context)
+        self.assertTrue('selecteduser' in self.response.context)
+        self.assertTrue('form' in self.response.context)
+        self.assertTrue('posts' in self.response.context)
+        self.assertTrue('users_list' in self.response.context)
+
+        contextDict = self.response.context
+
+        expected_medium_order = list(Medium.objects.order_by('-publish_date')[:5])
+        self.assertEqual(expected_medium_order, list(contextDict['posts']))
+
 
 def create_user_object():
     user = User.objects.get_or_create(username='testuser', first_name='Test', last_name='User', email='test@test.com')[0]
