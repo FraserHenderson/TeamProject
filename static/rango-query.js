f4d0b703777598122ccb2cd4ac87e8d397a03aa4
“Fraser <2557617h@student.gla.ac.uk>
2022-03-28 11:39:11 +0100

    more tweaks F&M

--- static/rango-query.js
+++ static/rango-query.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('.like_btn').click(function() {
-        var postIdVar;
-        postIdVar = $(this).attr('data-postid');
-        alert(postIdVar);
+// $(document).ready(function() {
+//     $('.like_btn').click(function() {
+//         var postIdVar;
+//         postIdVar = $(this).attr('data-postid');
+//         alert(postIdVar);
         
-        $.get('/rango/like_media/', {'post_id': postIdVar}, function(data) {
-                $('#like_count').html(data);
-                $('.like_btn').hide();
-            })
-    });
-});
+//         $.get('/rango/like_media/', {'post_id': postIdVar}, function(data) {
+//                 $('#like_count').html(data);
+//                 $('.like_btn').hide();
+//             })
+//     });
+// });
